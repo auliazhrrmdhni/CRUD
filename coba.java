42259da52129ab545fcf3b81078bc8e90876ce9e
Aulia Zahra Ramadhani <154733776+auliazhrrmdhni@users.noreply.github.com>
2025-03-08 11:10:16 +0800

    Add files via upload

---
+++ coba.java
@@ -0,0 +1,9 @@
+package com.example.demo1;
+
+public class coba {
+    public static void main(String[] args) {
+        Database db = new Database();
+        db.open();
+        db.view();
+    }
+}
